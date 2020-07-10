27ea2a0596698500311ebf67ab83dc21b4b78a73
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-07-10 15:15:10 -0400

    ex 4 test passing, MVP met

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,10 +2,20 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    negative = []
+    positive = {}
+    solution = []
     # Your code here
+    for num in a:
+        if num < 0:
+            negative.append(num)
+        else:
+            positive[num] = num
+    for number in negative:
+        if abs(number) in positive:
+            solution.append(positive[abs(number)])
+    return solution
 
-    return result
-
-
+    
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
